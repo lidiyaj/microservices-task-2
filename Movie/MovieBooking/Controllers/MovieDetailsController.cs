b017152af65a96f53ebd5d1eba33c51ab5667f27
Lidiya Joseph <lidiyajoseph@gmail.com>
2021-09-10 16:07:33 +0800

    Added Currency Conversion API

--- Movie/MovieBooking/Controllers/MovieDetailsController.cs
+++ Movie/MovieBooking/Controllers/MovieDetailsController.cs
@@ -2,10 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MovieBooking.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
 using System.Threading.Tasks;
 
 namespace MovieBooking.Controllers
@@ -109,6 +112,24 @@ namespace MovieBooking.Controllers
         private bool MovieDetailExists(long id)
         {
             return _context.MovieDetails.Any(e => e.Id == id);
+        }
+
+        [HttpGet]
+        [Route("ConvertCurrency/{from}/{to}")]
+        [ProducesResponseType(typeof(ConvertedValue), (int)HttpStatusCode.OK)]
+        [Produces(MediaTypeNames.Application.Json)]
+        public async Task<IActionResult> ConvertCurrency(string from, string to)
+        {
+            ConvertedValue value = new ConvertedValue();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"+ from.ToLower() + "/"+ to.ToLower() + ".json"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    value = JsonConvert.DeserializeObject<ConvertedValue>(apiResponse);
+                }
+            }
+            return Ok(value);
         }
     }
 }
---
+++ Movie/MovieBooking/Models/ConvertList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieBooking.Models
+{
+    public class ConvertedValue
+    {
+        public DateTime Date { get; set; }
+        public decimal SGD { get; set; }
+    }
+}
